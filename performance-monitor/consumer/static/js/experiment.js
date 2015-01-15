b2f3986c62e049e379e1bdc4d70be902e6797adb
dcvan <dcvan24@gmail.com>
2015-01-15 15:31:21 -0500

    experiments view update

--- performance-monitor/consumer/static/js/experiment.js
+++ performance-monitor/consumer/static/js/experiment.js
@@ -27,6 +27,7 @@ $(document).ready(function(){
 						+ ":" + m.getUTCMinutes() 
 						+ ":" + m.getUTCSeconds());
 			}
+			
 			for(i = 0; i < data.updates.length; i ++){
 				var step = data.updates[i].step;
 				if(!(step in cpuUsage))
@@ -51,18 +52,97 @@ $(document).ready(function(){
 					runtime[step] = [];
 				runtime[step].push(data.updates[i].runtime);
 			}
-			var cpuData = {
-					labels : labels,
-					datasets : []
-			}
+			var cr = 151, cg = 187, cb = 205;
+			var cpuData = {labels: labels, datasets: []},
+				memData = {labels: labels, datasets: []},
+				readCountData = {labels: labels, datasets: []},
+				writeCountData = {labels: labels, datasets: []},
+				readBytesData = {labels: labels, datasets: []},
+				writeBytesData = {labels: labels, datasets: []},
+				runtimeData = {labels: labels, datasets: []};
 			for(s in cpuUsage){
+				var cnt = +s;
+				color = '' + (cr + cnt * 10) + ',' + (cg - cnt * 10) + ',' + (cg - cnt * 15)
 				cpuData.datasets.push({
 					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
 					data: cpuUsage[s]
 				});
+				memData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: memUsage[s]
+				});
+				readCountData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: readCount[s]
+				});
+				writeCountData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: writeCount[s]
+				});
+				readBytesData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: readBytes[s]
+				});
+				writeBytesData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: writeBytes[s]
+				});
+				runtimeData.datasets.push({
+					label: s,
+		            fillColor: 'rgba('+ color +',0.2)',
+		            strokeColor: 'rgba('+ color + ',1)',
+		            pointColor: 'rgba(' + color ',1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba('+ color +',1)',
+					data: runtime[s]
+				});
+				
 			}
 			console.log(JSON.stringify(cpuData));
 			cpuChart.Line(cpuData, {bezierCurve: false});
+			memChart.Line(memData, {bezierCurve: false});
+			readCountChart.Line(readCountData, {bezierCurve: false});
+			writeCountChart.Line(writeCountData, {bezierCurve: false});
+			readBytesChart.Line(readBytesData, {bezierCurve: false});
+			writeBytesChart.Line(writeBytesData, {bezierCurve: false});
+			runtimeChart.Line(runtimeData, {bezierCurve: false});
 		}
 	});
 });
