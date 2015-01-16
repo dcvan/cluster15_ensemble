6afc5ecd089c4408e1811d823aa7d1cd44af1fba
dcvan <dcvan24@gmail.com>
2015-01-16 14:10:48 -0500

    ""

--- performance-monitor/consumer/static/js/experiment.js
+++ performance-monitor/consumer/static/js/experiment.js
@@ -19,121 +19,29 @@ $(document).ready(function(){
 						+ ":" + m.getUTCSeconds());
 			}
 			
-			var cr = 151, cg = 187, cb = 205;
-			
-			var walltimeData = {
-					labels: labels,
-					datasets: [
-					   {
-						   label: 'Walltime',
-						   fillColor: 'rgba('+ cr + ',' + cg + ',' + cb +',0.2)',
-				            strokeColor: 'rgba('+ cr + ',' + cg + ',' + cb + ',1)',
-				            pointColor: 'rgba(' + cr + ',' + cg + ',' + cb + ',1)',
-				            pointStrokeColor: '#fff',
-				            pointHighlightFill: '#fff',
-				            pointHighlightStroke: 'rgba(' + cr + ',' + cg + ',' + cb + ',1)',
-							data: data.walltime
-					   }
-					]
-				};
-			/*
-			for(s in cpuUsage){
-				var cnt = +s;
-				color = '' + (cr + cnt * 40)%255 + ',' + Math.abs(cg - cnt * 30)%255 + ',' + (cg + cnt * 75)%255
-				cpuData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: cpuUsage[s]
-				});
-				memData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: memUsage[s]
-				});
-				readCountData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: readCount[s]
-				});
-				writeCountData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: writeCount[s]
-				});
-				readBytesData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: readBytes[s]
-				});
-				writeBytesData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: writeBytes[s]
-				});
-				readRateData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: readRate[s]
-				});
-				writeRateData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: writeRate[s]
-				});
-				runtimeData.datasets.push({
-					label: s,
-		            fillColor: 'rgba('+ color +',0.2)',
-		            strokeColor: 'rgba('+ color + ',1)',
-		            pointColor: 'rgba(' + color + ',1)',
-		            pointStrokeColor: '#fff',
-		            pointHighlightFill: '#fff',
-		            pointHighlightStroke: 'rgba('+ color +',1)',
-					data: runtime[s]
-				});
-			}
-			*/
-			walltimeChart.Line(walltimeData, {bezierCurve: false});
-			legend(document.getElementById('walltime_legend'), walltimeData);
+			plotLine(walltimeChart, labels, 'Walltime', data.walltime, document.getElementById('walltime_legend'));
 		}
 	});
 });
+
+function plotLine(chart, labels, label, data, lengend_area){
+	var color = 'rgba(151, 187, 205';
+	var lineData = {
+			labels: labels,
+			datasets: [
+			   {
+				   label: label,
+				   fillColor: color +',0.2)',
+		           strokeColor: color + ',1)',
+		           pointColor: color + ',1)',
+		           pointStrokeColor: '#fff',
+		           pointHighlightFill: '#fff',
+		           pointHighlightStroke: color + ',1)',
+		           data: data
+			   }
+			]
+	};
+	
+	chart.Line(data, {bezierCurve: false});
+	legend(legend_area, lineData);
+}
