08d37269ea7a7a398d7120ebc837760cd7b5e869
dcvan <dcvan24@gmail.com>
2015-01-24 05:47:09 -0500

    removed heartbeat

--- performance-monitor/producer/config.py
+++ performance-monitor/producer/config.py
@@ -2,6 +2,6 @@ RECONNECT_INT = 1
 
 EXCHANGE_NAME = 'cluster15_stat'
 
-MESSAGE_BROKER_URI = 'amqp://dc:cluster15@152.54.14.32:5672/%2F?heartbeat_interval=20'
+MESSAGE_BROKER_URI = 'amqp://dc:cluster15@152.54.14.32:5672/%2F'
 
 CONDOR_EXE_DIR = '/var/lib/condor/execute'
